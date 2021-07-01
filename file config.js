a3060e5e703fb4dbe0848759c5fa3d1188f4197e
Viniteck <86772332+Viniteck@users.noreply.github.com>
2021-07-01 15:52:48 +0700

    Update and rename config.js to file config.js

---
+++ file config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Yew tuiii honggg:3',   
+    desc: 'Chưa trả lời mà thoát là tui bắt zề nuôi áhh ',
+    btnYes: 'Yew thì yew <33',
+    btnNo: 'Không ạ :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yew hong cần lí do đc hong <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều bbi của anh �😻😽',
+    messDesc: 'Yew công chúa của anhhh.',
+    btnAccept: 'Dạaaaaa <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
